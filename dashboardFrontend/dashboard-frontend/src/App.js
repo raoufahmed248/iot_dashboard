071a5c427ad1f7fa0de907d0b307ce3d3f83855f
Mohammed <raouf.ahmed248@gmail.com>
2021-03-12 14:21:45 -0600

    added temperature fetching

--- dashboardFrontend/dashboard-frontend/src/App.js
+++ dashboardFrontend/dashboard-frontend/src/App.js
@@ -5,28 +5,97 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import {LineChart, BarChart, DoughnutChart, getData, getRandomArray, getRandomDateArray} from "./Component/Dashboard/index.js";
+import axios from 'axios';
 
+import {LineChart, BarChart, DoughnutChart} from "./Component/Dashboard/index.js";
+// Data generation
+function getRandomArray(numItems) {
+  // Create random array of objects
+  let names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let data = [];
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      label: names[i],
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
+
+function getRandomDateArray(numItems) {
+  // Create random array of objects (with date)
+  let data = [];
+  let baseTime = new Date('2018-05-01T00:00:00').getTime();
+  let dayMs = 24 * 60 * 60 * 1000;
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      time: new Date(baseTime + i * dayMs),
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
+
+
+
+function getData() {
+  let data = [];
+
+  data.push({
+    title: 'Temperature (F)',
+    data: getRandomDateArray(150)
+  });
+
+  data.push({
+    title: 'Humidity (%)',
+    data: getRandomArray(20)
+  });
+
+  data.push({
+    title: 'Pressure (kPa)',
+    data: getRandomArray(10)
+  });
+
+  data.push({
+    title: 'Data 4',
+    data: getRandomArray(6)
+  });
+
+  return data;
+}
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: getData()
+      data: getData(),
+      temperatures: [],
+      humidities: [],
+      pressures: [],  
     };
   }
-
+  getTemperatureData() {
+    let outputData = [];
+    axios.get("http://localhost:8000/temperatures/")
+      .then( (response) =>{
+        console.log(response.data)
+        this.setState({temperatures:response.data})
+      });
+  }
+  
   componentDidMount() {
+    this.getTemperatureData();
     window.setInterval(() => {
       this.setState({
         data: getData()
       })
     }, 5000)
   }
+  
   render() {
     return (
         <div className="App">
-          <div className="main chart-wrapper">
+          <div className="sub chart-wrapper">
             <LineChart
               data={this.state.data[0].data}
               title={this.state.data[0].title}
@@ -34,20 +103,20 @@ class App extends Component {
             />
           </div>
           <div className="sub chart-wrapper">
-            <BarChart
-              data={this.state.data[1].data}
-              title={this.state.data[1].title}
-              color="#70CAD1"
+            <LineChart
+              data={this.state.data[0].data}
+              title={this.state.data[0].title}
+              color="#3E517A"
             />
           </div>
           <div className="sub chart-wrapper">
-            <BarChart
-              data={this.state.data[2].data}
-              title={this.state.data[2].title}
-              color="#B08EA2"
+            <LineChart
+              data={this.state.data[0].data}
+              title={this.state.data[0].title}
+              color="#3E517A"
             />
           </div>
-          <div className="sub chart-wrapper">
+          <div className="main chart-wrapper">
             <DoughnutChart
               data={this.state.data[3].data}
               title={this.state.data[3].title}
--- dashboardFrontend/dashboard-frontend/src/Component/Dashboard/index.js
+++ dashboardFrontend/dashboard-frontend/src/Component/Dashboard/index.js
@@ -2,59 +2,7 @@ import React, { Component } from 'react';
 import Chart from 'chart.js'
 Chart.defaults.global.defaultFontFamily = "Roboto, sans-serif";
 
-// Data generation
-function getRandomArray(numItems) {
-  // Create random array of objects
-  let names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let data = [];
-  for(var i = 0; i < numItems; i++) {
-    data.push({
-      label: names[i],
-      value: Math.round(20 + 80 * Math.random())
-    });
-  }
-  return data;
-}
 
-function getRandomDateArray(numItems) {
-  // Create random array of objects (with date)
-  let data = [];
-  let baseTime = new Date('2018-05-01T00:00:00').getTime();
-  let dayMs = 24 * 60 * 60 * 1000;
-  for(var i = 0; i < numItems; i++) {
-    data.push({
-      time: new Date(baseTime + i * dayMs),
-      value: Math.round(20 + 80 * Math.random())
-    });
-  }
-  return data;
-}
-
-function getData() {
-  let data = [];
-
-  data.push({
-    title: 'Visits',
-    data: getRandomDateArray(150)
-  });
-
-  data.push({
-    title: 'Categories',
-    data: getRandomArray(20)
-  });
-
-  data.push({
-    title: 'Categories',
-    data: getRandomArray(10)
-  });
-
-  data.push({
-    title: 'Data 4',
-    data: getRandomArray(6)
-  });
-
-  return data;
-}
 
 // BarChart
 class BarChart extends React.Component {
@@ -198,7 +146,7 @@ class DoughnutChart extends React.Component {
   }
 }
 
-export {LineChart, BarChart, DoughnutChart, getData, getRandomDateArray, getRandomArray};
+export {LineChart, BarChart, DoughnutChart};
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
